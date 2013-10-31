807624bbf9c8274714ddd98356d1c3163b43b3c8
David Tjokroaminoto <dynamic.dante@gmail.com>
2013-10-31 22:54:00 +0800

    Added Fields in Admin and Model

--- logger/admin.py
+++ logger/admin.py
@@ -5,10 +5,10 @@ class TorrentAdmin(admin.ModelAdmin):
 	list_display = ('torrent_hash','title','category','filename','published','spidered')
 
 class PeerAdmin(admin.ModelAdmin):
-	list_display = ('ip_address','country','client_type')
+	list_display = ('ip_address','country','client_type','host')
 	
 class ActivityAdmin(admin.ModelAdmin):
-	list_display= ('torrent','peer','download_speed','upload_speed')
+	list_display= ('torrent','peer','flags','downloaded','download_speed','upload_speed')
 
 admin.site.register(Torrent, TorrentAdmin)
 admin.site.register(Peer, PeerAdmin)
--- logger/models.py
+++ logger/models.py
@@ -4,27 +4,30 @@ class Peer(models.Model):
 	ip_address = models.CharField(max_length=64)
 	country = models.CharField(max_length=64, blank=True)
 	client_type = models.CharField(max_length=64, blank=True)
+	host = models.CharField(max_length=250, blank=True)
 
 	def __unicode__(self):
 		return self.ip_address
 
 class Torrent(models.Model):
 	torrent_hash = models.CharField(max_length=250)
-	title = models.CharField(max_length=250)
-	category = models.CharField(max_length=250, blank=True)
-	filename = models.CharField(max_length=250, blank=True)
-	published = models.DateField(blank=True, null=True)
-	spidered = models.DateTimeField(blank=True, null=True)
-	peers = models.ManyToManyField(Peer, through='Activity')
+	title 		= models.CharField(max_length=250)
+	category 	= models.CharField(max_length=250, blank=True)
+	filename 	= models.CharField(max_length=250, blank=True)
+	published 	= models.DateField(blank=True, null=True)
+	spidered 	= models.DateTimeField(blank=True, null=True, auto_now_add=True)
+	peers 		= models.ManyToManyField(Peer, through='Activity')
 
 	def __unicode__(self):
 		return self.title
 
 class Activity(models.Model):
-	torrent = models.ForeignKey(Torrent)
-	peer = models.ForeignKey(Peer)
-	download_speed = models.IntegerField(blank=True, null=True)
-	upload_speed = models.IntegerField(blank=True, null=True)
+	torrent 		= models.ForeignKey(Torrent)
+	peer 			= models.ForeignKey(Peer)
+	flags 			= models.CharField(max_length=250, blank=True)
+	downloaded 		= models.CharField(max_length=250, blank=True)
+	download_speed 	= models.IntegerField(blank=True, null=True)
+	upload_speed 	= models.IntegerField(blank=True, null=True)
 
 	def __unicode__(self):
 		return self.torrent.title + ' : ' + self.peer.ip_address
